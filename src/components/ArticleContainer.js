d0669a77d4d3ae0a172144cb6998d6dd80e51326
Randy Burnet <rrw.burnet@gmail.com>
2019-07-26 16:17:33 +0200

    added buttons

--- src/components/ArticleContainer.js
+++ src/components/ArticleContainer.js
@@ -1,5 +1,7 @@
 import React from "react"
 import styled from "styled-components"
+import { Link } from "gatsby"
+
 import ArticleItem from "./ArticleItem"
 
 const ArticleItemsWrapper = styled.div`
@@ -29,15 +31,37 @@ const ArticleItemsWrapperMoreBtn = styled.button`
   margin-top: 20px;
 `
 
+const ArticleBtn = styled.button`
+  color: white;
+  border-radius: 6px;
+  text-decoration: none;
+  border: none;
+  background-color: #d8d8d8;
+  box-shadow: 0 2px 16px 0 rgba(0, 0, 0, 0.12), 0 0 1px 0 rgba(0, 0, 0, 0.08);
+  margin: 30px 0px;
+  max-width: 300px;
+  align-self: center;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+
+const ReadMoreLink = styled(Link)`
+  display: block;
+  justify-content: center;
+  text-align: center;
+`
+
 const ArticleContainer = () => {
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <ArticleItemsWrapperTitle>Articles</ArticleItemsWrapperTitle>
       <ArticleItemsWrapper>
         <ArticleItem />
-        {/* <ArticleItemsWrapperMoreBtn>Read More</ArticleItemsWrapperMoreBtn> */}
       </ArticleItemsWrapper>
-    </>
+      <ReadMoreLink to={"/article"}> <ArticleBtn>ALL ARTICLES</ArticleBtn></ReadMoreLink>
+    </div>
   )
 }
 
--- src/components/StoreSection.js
+++ src/components/StoreSection.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import styled from 'styled-components'
-import bookImg from '../images/book.png';
+import React from "react"
+import styled from "styled-components"
+import bookImg from "../images/book.png"
 
 const StoreWrapper = styled.div`
-padding: 0px 20px;
-margin-top: 50px;
-margin-bottom: 50px;
+  padding: 0px 20px;
+  margin-top: 50px 0px;
   width: 100%;
-  display: flex;
-  justify-content: center;
+  display: flex; 
   flex-direction: column;
   position: relative;
   text-align: center;
 `
 
 const StoreImage = styled.img`
-padding: 0px 0px;
- height: 500px;
- max-width: 350px;
- align-self: center;
- box-shadow: 0 2px 16px 0 rgba(0, 0, 0, 0.12), 0 0 1px 0 rgba(0, 0, 0, 0.08);
-
+  padding: 0px 0px;
+  height: 500px;
+  max-width: 350px;
+  align-self: center;
+  box-shadow: 0 2px 16px 0 rgba(0, 0, 0, 0.12), 0 0 1px 0 rgba(0, 0, 0, 0.08);
 `
 
 const StoreBtn = styled.button`
@@ -33,16 +30,42 @@ const StoreBtn = styled.button`
   margin-top: 20px;
   align-self: center;
 `
+const OrderBtn = styled.button`
+  color: white;
+  border-radius: 6px;
+  text-decoration: none;
+  border: none;
+  background-color: #d8d8d8;
+  box-shadow: 0 2px 16px 0 rgba(0, 0, 0, 0.12), 0 0 1px 0 rgba(0, 0, 0, 0.08);
+  margin-bottom: 150px;
+  max-width: 300px;
+  align-self: center;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+
+const StoreText = styled.div`
+
+`
 
 const StoreSection = () => {
-  return(
+  return (
     <StoreWrapper>
       <h2>FOR THE CULURE MAGAZINE OUT NOW !</h2>
-      <br/>
-      <StoreImage  src={bookImg}/>
       <br />
-      <p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
-      <StoreBtn>ORDER HERE</StoreBtn>
+      <StoreImage src={bookImg} />
+      <br />
+      <StoreText>
+      <p>
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse.
+      </p>
+      </StoreText>
+      <OrderBtn>ORDER HERE</OrderBtn>
     </StoreWrapper>
   )
 }
